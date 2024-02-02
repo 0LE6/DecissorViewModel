9ad840abc958a5d48f8c6306afd35925ace72715
Oleg <kharenkoleg@gmail.com>
2024-02-02 20:28:02 +0100

    update --

--- app/src/main/java/cat/institutmontivi/decissorviewmodel/dades/PreferenciesDataStore.kt
+++ app/src/main/java/cat/institutmontivi/decissorviewmodel/dades/PreferenciesDataStore.kt
@@ -25,12 +25,42 @@ class PreferenciesDataStore(private val context: Context) {
     }
 
     val getTempsCaraOCreo: Flow<Long> = context.datastore.data.map {
-        preferencies -> preferencies[TEMPS_CARA_O_CREU] ?: 0
+        preferencies -> preferencies[TEMPS_CARA_O_CREU] ?: 0 // hay que poner un valor por defecto para la primera vez
     }
 
     suspend fun saveTempsCaraOCreu(valor: Long) {
         context.datastore.edit {
                 preferencies -> preferencies[TEMPS_CARA_O_CREU] = valor
+        }
+    }
+
+    val getMinimTriaNumero: Flow<Int> = context.datastore.data.map {
+        preferencies -> preferencies[MINIM_TRIA_NUMERO] ?: 0 // hay que poner un valor por defecto para la primera vez
+    }
+
+    suspend fun saveMinimTriaNumero(valor: Int) {
+        context.datastore.edit {
+                preferencies -> preferencies[MINIM_TRIA_NUMERO] = valor
+        }
+    }
+
+    val getMaximTriaNumero: Flow<Int> = context.datastore.data.map {
+        preferencies -> preferencies[MAXIM_TRIA_NUMERO] ?: 0 // hay que poner un valor por defecto para la primera vez
+    }
+
+    suspend fun saveMaximTriaNumero(valor: Int) {
+        context.datastore.edit {
+                preferencies -> preferencies[MAXIM_TRIA_NUMERO] = valor
+        }
+    }
+
+    val getPreguntaPerDefecte: Flow<String> = context.datastore.data.map {
+        preferencies -> preferencies[PREGUNTA_PER_DEFECTE] ?: "" // hay que poner un valor por defecto para la primera vez
+    }
+
+    suspend fun savePreguntaPerDefecte(valor: String) {
+        context.datastore.edit {
+                preferencies -> preferencies[PREGUNTA_PER_DEFECTE] = valor
         }
     }
 }
