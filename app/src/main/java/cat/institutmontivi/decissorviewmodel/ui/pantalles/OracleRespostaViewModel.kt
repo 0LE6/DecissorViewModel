05335cd9e9d265bef3af2899e8ca9e2c60d7300b
Oleg <kharenkoleg@gmail.com>
2024-02-02 19:44:03 +0100

    update --

--- app/src/main/java/cat/institutmontivi/decissorviewmodel/ui/pantalles/OracleRespostaViewModel.kt
+++ app/src/main/java/cat/institutmontivi/decissorviewmodel/ui/pantalles/OracleRespostaViewModel.kt
@@ -1,9 +1,18 @@
 package cat.institutmontivi.decissorviewmodel.ui.pantalles
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class OracleRespostaViewModel : ViewModel() {
 
+    var estat by mutableStateOf(OracleRespostaEstat())
+        private set
+
+    init {
+        estat = estat.copy()
+    }
     data class OracleRespostaEstat(
         val resposta: String = ""
     )
