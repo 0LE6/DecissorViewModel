ba6be68fdfda391cf513e2b137ecde5585192b84
Oleg <kharenkoleg@gmail.com>
2024-02-02 20:16:07 +0100

    update --

--- app/src/main/java/cat/institutmontivi/decissorviewmodel/dades/PreferenciesDataStore.kt
+++ app/src/main/java/cat/institutmontivi/decissorviewmodel/dades/PreferenciesDataStore.kt
@@ -3,6 +3,9 @@ package cat.institutmontivi.decissorviewmodel.dades
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.longPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 
 // NOTE : los datos se guardan en la parte privada de la aplicación, para ello se necesito el Context
@@ -10,6 +13,11 @@ class PreferenciesDataStore(private val context: Context) {
 
     companion object {
 
-        private val Context.datastore: DataStore<Preferences> by preferencesDataStore(("preferencies"))
+        private val Context.datastore: DataStore<Preferences>
+            by preferencesDataStore(("preferencies"))
+        private val TEMPS_CARA_O_CREU = longPreferencesKey("tempsCaraOCreu")
+        private val MINIM_TRIA_NUMERO = intPreferencesKey("minimTriaNumero")
+        private val MAXIM_TRIA_NUMERO = intPreferencesKey("maximTriaNumero")
+        private val PREGUNTA_PER_DEFECTE = stringPreferencesKey("preguntaPerDefecte")
     }
 }
