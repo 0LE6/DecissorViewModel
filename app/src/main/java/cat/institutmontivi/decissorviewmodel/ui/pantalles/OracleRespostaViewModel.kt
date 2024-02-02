e403ba66bdce5f2d9eda74b9eeb25c5aa8e1de74
Oleg <kharenkoleg@gmail.com>
2024-02-02 19:42:28 +0100

    update -- arnau

---
+++ app/src/main/java/cat/institutmontivi/decissorviewmodel/ui/pantalles/OracleRespostaViewModel.kt
@@ -0,0 +1,10 @@
+package cat.institutmontivi.decissorviewmodel.ui.pantalles
+
+import androidx.lifecycle.ViewModel
+
+class OracleRespostaViewModel : ViewModel() {
+
+    data class OracleRespostaEstat(
+        val resposta: String = ""
+    )
+}
