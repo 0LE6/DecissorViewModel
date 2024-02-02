12fcc930ed047f402c6fcabbebe3e7a09687ca85
Oleg <kharenkoleg@gmail.com>
2024-02-02 18:50:19 +0100

    add -- import importante para el ciclo de vida

---
+++ app/src/main/java/cat/institutmontivi/decissorviewmodel/ui/pantalles/CaraOCreuViewModel.kt
@@ -0,0 +1,7 @@
+package cat.institutmontivi.decissorviewmodel.ui.pantalles
+
+import androidx.lifecycle.ViewModel // NOTE : importante para el concepto de ciclo de vida
+
+class CaraOCreuViewModel : ViewModel() {
+
+}
