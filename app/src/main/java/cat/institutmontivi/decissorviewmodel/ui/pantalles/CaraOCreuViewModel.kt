d18fab3776c1d57604c6f73a4ae242ba8dcace8b
Oleg <kharenkoleg@gmail.com>
2024-02-02 19:00:34 +0100

    add --

--- app/src/main/java/cat/institutmontivi/decissorviewmodel/ui/pantalles/CaraOCreuViewModel.kt
+++ app/src/main/java/cat/institutmontivi/decissorviewmodel/ui/pantalles/CaraOCreuViewModel.kt
@@ -1,9 +1,18 @@
 package cat.institutmontivi.decissorviewmodel.ui.pantalles
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel // NOTE : importante para el concepto de ciclo de vida
 
 class CaraOCreuViewModel : ViewModel() {
 
+    var estat by mutableStateOf(CaraOCreuEstat())
+        private set
+
+    init { // NOTE : inicializar
+        estat = estat.copy(resultat = 0, temps = 0)
+    }
     data class CaraOCreuEstat (
         val resultat: Int = 0,
         val temps: Long = 1000
